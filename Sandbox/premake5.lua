c5f62f138c87cd0182d6f028439395c437c0530f
Nasir Hossain <kitcodail@gmail.com>
2023-05-24 23:30:56 -0500

    Resolved some code duplication with lua function

--- Sandbox/premake5.lua
+++ Sandbox/premake5.lua
@@ -1,18 +1,4 @@
-project "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
 
-    -- Set the project-specific source and header files
-    files { "src/*.cpp", "src/*.h" }
-
-    -- Link against the Floraft static library and the SFML libraries
-    links { "Floraft", "sfml-graphics", "sfml-window", "sfml-system" }
-
-    filter "configurations:Debug"
-        symbols "On"
-        defines { "DEBUG" }
-
-    filter "configurations:Release"
-        optimize "On"
-        defines { "RELEASE" }
+DefaultProject("Sandbox", "ConsoleApp")
+	links { "Floraft", "sfml-graphics", "sfml-window", "sfml-system" }
 
--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,34 @@
+
+ -- The main workflow
+function DefaultProject(name, type)
+	project(name)
+	kind(type)
+	language "C++"
+
+	files { "src/*.cpp", "src/*.h" }
+
+	filter "configurations:Debug"
+		symbols "On"
+		defines { "DEBUG" }
+
+	filter "configurations:Release"
+		optimize "On"
+		defines { "RELEASE" }
+end
+
+
 workspace "FloraftEngine"
-    architecture "x86_64"
-    configurations { "Debug", "Release" }
+	architecture "x86_64"
+	configurations { "Debug", "Release" }
 
-    -- Enable C++17 support
-    cppdialect "C++17"
+	-- Enable C++17 support
+	cppdialect "C++17"
 
-    -- Add the SFML include and library directories
-    includedirs { "/usr/include", "Floraft/src" }
-    libdirs { "/usr/lib" }
+	-- Add the SFML include and library directories
+	includedirs { "/usr/include", "Floraft/src" }
+	libdirs { "/usr/lib" }
 
+
+ -- Include premake5.lua from Floraft and Sandbox folder
 include "Floraft"
 include "Sandbox"
